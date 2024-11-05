9338eda737c3c0fa3f5366822e3281b70dbbc7e5
Algus <algukenan@gmail.com>
2024-11-05 14:15:23 +0300

    Addition and Subtraction
    Addition and Subtraction with using class and methods.

---
+++ Methods_In_Class_With_Return/Program.cs
@@ -0,0 +1,26 @@
+﻿using Methods_In_Class_With_Return;
+
+islem isl = new islem();
+
+int A, B;
+
+Console.WriteLine("İşlem Seçiniz:" + "\n" + "1- Toplama " + "2- Çıkartma");
+
+int islemsec = Convert.ToInt32(Console.ReadLine());
+
+if (islemsec == 1)
+{
+    Console.WriteLine("Toplamak istediğiniz sayıları giriniz.");
+    A = Convert.ToInt32(Console.ReadLine());
+    B = Convert.ToInt32(Console.ReadLine());
+    isl.topla(A, B);
+}
+else if(islemsec == 2)
+{
+    Console.WriteLine("Çıkartmak istediğiniz sayıları giriniz.");
+    A = Convert.ToInt32(Console.ReadLine());
+    B = Convert.ToInt32(Console.ReadLine());
+    isl.cikart(A, B);
+}
+
+Console.Read();
---
+++ Methods_In_Class_With_Return/islem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_In_Class_With_Return
+{
+    internal class islem
+    {
+        public int topla(int a , int b)
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+            return c;
+        }
+        
+        public int cikart(int a , int b)
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+            return c;
+        }
+    }
+}
